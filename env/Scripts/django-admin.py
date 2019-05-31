e408de897e0e3b3bf0d6807a8e3f709c1b16b79a
Jerome Janier <jeromejanier2@gmail.com>
2019-05-31 10:59:45 +0800

    add new ignorelist

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!f:\programming projects\django\laugo_management\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
